a9c13b18ac4aee5f2f766d1ef150b2045589f90e
Iurii Bairamukov <juribai@mail.ru>
2020-10-30 18:14:59 +0700

    feat: создать компонент Wellcome

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,19 @@
 import './App.scss';
 import Form from './Form';
+import Wellcome from './Wellcome';
 
 function App() {
   return (
     <div className="app">
-      <h1 className="title">
-        Здравствуйте,{' '}
-        <span className="title__user">
-          Человек №{Math.floor(Math.random() * 1000)}
-        </span>
-      </h1>
-      <Form />
+      <header className="header">
+        <Wellcome
+          classSelector="header__wrapper"
+          user={`Человек №${Math.floor(Math.random() * 1000)}`}
+        />
+      </header>
+      <main className="main">
+        <Form />
+      </main>
     </div>
   );
 }
