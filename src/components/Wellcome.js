9ce7f4bd9fd46d6627be70c0e37a65d6827facc2
Iurii Bairamukov <juribai@mail.ru>
2020-11-01 12:55:10 +0700

    feat: добавитт стили к Wellcome

--- src/components/Wellcome.js
+++ src/components/Wellcome.js
@@ -5,12 +5,10 @@ import './Wellcome.scss';
 const Wellcome = ({ classSelector, user }) => {
   return (
     <div className={`${classSelector} wellcome`}>
-      <div className="wellcome__title">
-        <h1 className="wellcome__user-title">
-          Здравствуйте, <span className="wellcome__user-current">{user}</span>
-        </h1>
-        <Button classSelector="wellcome__button">Сменить статус</Button>
-      </div>
+      <h1 className="wellcome__user-title">
+        Здравствуйте, <span className="wellcome__user-current">{user}</span>
+      </h1>
+      <Button classSelector="wellcome__button">Сменить статус</Button>
       <div className="wellcome__status">
         <p className="wellcome__status-text">
           Прежде чем действовать, надо понять
@@ -18,8 +16,9 @@ const Wellcome = ({ classSelector, user }) => {
         <input
           name="status"
           type="text"
-          className="wellcome__status-input"
+          className="wellcome__status-input wellcome__status-input_display_hidden"
         ></input>
+        <div className="wellcome__square-status"></div>
       </div>
     </div>
   );
