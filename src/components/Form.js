244330270ab438e77e952cdca47acabbd539aaed
Iurii Bairamukov <juribai@mail.ru>
2020-11-01 17:28:44 +0700

    feat: добавить стили к Form

--- src/components/Form.js
+++ src/components/Form.js
@@ -9,39 +9,51 @@ import './Form.scss';
 const Form = () => {
   return (
     <form className="form form__container">
-      <fieldset className="form__input-container form__input-container_ctrl_city">
-        <FormSelect title="Ваш город" selectList={cityList} />
+      <fieldset className="form__fieldset form__fieldset_ctrl_city">
+        <FormSelect
+          formSelector="form__input-container_ctrl_city"
+          title="Ваш город"
+          name="userCity"
+          label="user-city"
+          classSelector="form__item_city"
+          selectList={cityList}
+        />
       </fieldset>
 
-      <fieldset className="form__input-container form__input-container_ctrl_password">
+      <fieldset className="form__fieldset form__fieldset_ctrl_password">
         <FormInput
+          formSelector="form__input-container_ctrl_password"
           title="Пароль"
           name="password"
           label="password"
           classSelector="password"
           type="password"
+          prompt="Ваш новый пароль должен содержать не менее 5 символов."
         />
         <FormInput
+          formSelector="form__input-container_ctrl_password"
           title="Пароль еще раз"
           name="passwordRepeat"
           label="password-repeat"
           classSelector="password"
           type="password"
+          prompt="Повторите пароль, пожалуйста, это обезопасит вас с нами на случай ошибки."
         />
       </fieldset>
 
-      <fieldset className="form__input-container form__input-container_ctrl_email">
+      <fieldset className="form__fieldset form__fieldset_ctrl_email">
         <FormInput
+          formSelector="form__input-container_ctrl_email"
           title="Электронная почта"
           name="userEmail"
           label="user-email"
           classSelector="email"
           type="email"
+          prompt="Можно изменить адрес, указанный при регистрации."
         />
-      </fieldset>
 
-      <fieldset className="form__input-container form__input-container_ctrl_agreement">
         <FormCheckBox
+          formSelector="form__input-container_ctrl_email-check"
           title="Я согласен"
           name="userAgreement"
           label="user-agreement"
--- src/components/FormCheckBox.js
+++ src/components/FormCheckBox.js
@@ -1,22 +1,31 @@
 import React from 'react';
 
-const FormCheckBox = ({ title, name, label, classSelector, about }) => {
+const FormCheckBox = ({
+  formSelector,
+  title,
+  name,
+  label,
+  classSelector,
+  about,
+}) => {
   return (
-    <>
+    <div className={`form__input-container ${formSelector}`}>
       <label className="form__label" htmlFor={label}>
-        {title}
+        {title || ''}
       </label>
-      <input
-        id={label}
-        name={name}
-        className={classSelector}
-        type="checkbox"
-        required
-      ></input>
-      <label className="form__label" htmlFor={label}>
-        {about}
-      </label>
-    </>
+      <div className="form__item-wrapper">
+        <input
+          id={label}
+          name={name}
+          className={`form__item form__item_${classSelector}`}
+          type="checkbox"
+        ></input>
+        <label className="form__label form__checkbox-about" htmlFor={label}>
+          {about}
+        </label>
+        <span class="visible-checkbox"></span>
+      </div>
+    </div>
   );
 };
 
