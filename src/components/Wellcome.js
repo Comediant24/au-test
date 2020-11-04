12d361833e2cbf5315c85e70782a1c85b07e35ba
Iurii Bairamukov <juribai@mail.ru>
2020-11-04 17:51:40 +0700

    feat: создать функционал для изменения статуса

--- src/components/Wellcome.js
+++ src/components/Wellcome.js
@@ -1,23 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from './Button';
 import './Wellcome.scss';
 
 const Wellcome = ({ classSelector, user }) => {
+  const [status, setStatus] = useState('Прежде чем действовать, надо понять');
+  const [isShow, setIsShow] = useState(false);
+  const [buttonTitle, setButtonTitle] = useState('Сменить статус');
+
+  const handleClick = (e) => {
+    setIsShow(!isShow);
+    setButtonTitle(isShow ? 'Изменить статус' : 'Сохранить');
+  };
+
+  const handleChange = (e) => {
+    setStatus(e.target.value);
+  };
+
   return (
     <div className={`${classSelector} wellcome`}>
       <h1 className="wellcome__user-title">
         Здравствуйте, <span className="wellcome__user-current">{user}</span>
       </h1>
-      <Button classSelector="wellcome__button">Сменить статус</Button>
+      <Button
+        onClick={handleClick}
+        disabled="false"
+        classSelector="wellcome__button"
+      >
+        {buttonTitle}
+      </Button>
       <div className="wellcome__status">
-        <p className="wellcome__status-text">
-          Прежде чем действовать, надо понять
-        </p>
-        <input
-          name="status"
-          type="text"
-          className="wellcome__status-input wellcome__status-input_display_hidden"
-        ></input>
+        {isShow ? (
+          <input
+            autoFocus
+            maxLength="120"
+            value={status}
+            onChange={handleChange}
+            name="status"
+            type="text"
+            className={`wellcome__status-input wellcome__status-text${
+              isShow ? '' : 'wellcome__status-input_display_hidden'
+            }`}
+          ></input>
+        ) : (
+          <p className="wellcome__status-text"> {status || 'Ваш статус'}</p>
+        )}
+
         <div className="wellcome__square-status"></div>
       </div>
     </div>
